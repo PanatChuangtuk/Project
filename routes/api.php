45ff29dde0118b3553af660bfad5ffa78a5e82a6
PanatChuangtuk <panat32@hotmail.com>
2025-01-29 14:40:37 +0700

    update api

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\ERPWebhookLogController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/erp-webhook', [ERPWebhookLogController::class, 'index'])->name('erp-webhook');
+// Route::post('/erp-webhook/update/{id}', [ERPWebhookLogController::class, 'update'])->name('erp-webhook.update');
+// Route::get('/erp-webhook/delete/{id}', [ERPWebhookLogController::class, 'delete'])->name('erp-webhook.del');
+
+
+// Route::prefix('erp-webhook')->group(function () {
+//     Route::controller(ERPWebhookLogController::class)->group(function () {
+//         Route::post('/', 'index');
+//         Route::patch('/update/{id}', 'update');
+//         Route::delete('/delete/{id}', 'delete');
+//     });
+// });
