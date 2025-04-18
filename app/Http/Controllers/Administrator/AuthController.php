331518e633ff2780cb4cf5dff73e413c2fa8a664
PanatChuangtuk <panat32@hotmail.com>
2025-04-18 17:46:51 +0700

    Update AuthController.php

--- app/Http/Controllers/Administrator/AuthController.php
+++ app/Http/Controllers/Administrator/AuthController.php
@@ -8,24 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    // public function login()
-    // {
-    //     return view('administrator.auth.login');
-    // }
-
-    // public function loginPost(Request $request)
-    // {
-    //     $credetials = [
-    //         'email' => $request->email,
-    //         'password' => $request->password,
-    //     ];
-
-    //     if (Auth::guard('web')->attempt($credetials)) {
-    //         return redirect('/administrator/dashboard')->with('success', 'Login berhasil');
-    //     }
-    //     return back()->with('error', 'Email or Password salah');
-    // }
-
     public function logout()
     {
         Auth::guard('web')->logout();
