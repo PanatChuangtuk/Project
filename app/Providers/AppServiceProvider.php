00e1d647a06e47e08b99261768398e94799add46
PanatChuangtuk <panat32@hotmail.com>
2025-05-21 16:27:59 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,16 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\{View, URL};
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $host = request()->getHost();
+        if (app()->environment('production') || str_contains($host, 'ngrok') || str_contains($host, 'amazonaws')) {
+            URL::forceScheme('https');
+        }
         Paginator::defaultView('administrator.pagination');
     }
 }
